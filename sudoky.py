ec6ff0546225b0345b91860538d37bfee9b00d3c
Martim Baltazar <84470530+MartimBaltazar@users.noreply.github.com>
2021-11-15 22:05:20 +0000

    Update Solver
    Condições do solver a funcionar

--- sudoky.py
+++ sudoky.py
@@ -22,37 +22,40 @@ def get_coluna(tab, n):
 
 def get_celula(tab, l1,c1):
     cel = []
+    il = 0
          
-    if 0<l1<3:
+    if 0 <= l1 < 3:
         il = 0 
         jl = 3
-    if 0<c1<3:
+    if 0 <= c1 < 3:
         ic = 0 
         jc = 3
-    if 3<=l1<6:
+    if 3 <= l1 < 6:
         il = 3 
         jl = 6
         
-    if 3<=c1<6:
+    if 3 <= c1 < 6:
         ic = 3 
         jc = 6
     
-    if 6<l1<=8:
+    if 6 <= l1 <=8:
         il = 6 
         jl = 9
         
-    if 6<c1<=8:
+    if 6 <= c1 <= 8:
         ic = 6 
         jc = 9
+    
+    
         
     for a in range(il,jl):
         for b in range(ic,jc):
             cel.append(tab[a][b])
      
-    print(cel)
+    
     return cel
    
-get_celula(A,5,5)
+#get_celula(A,5,5)
     
 
 def solver(tab):
@@ -62,11 +65,10 @@ def solver(tab):
         
         for b in range(0, len(tab)):
             
+            
             if ((tab[a][b] == 0)):
-                print(tab[a])
                 
-                while (i in tab[a]) or (i in get_coluna(tab,a)) or (i in get_celula(tab,a,b)) :
-                    
+                while (i in tab[a]) or (i in get_coluna(tab,b)) or (i in get_celula(tab,a,b)) :
                     i+=1    
                 
                 if (i <= 9):
@@ -80,12 +82,11 @@ def solver(tab):
             
 
 
-
 def get_tabuleiro(tab):
     puzzle = ''
     state = False
     for a in range(0, len(tab)): #percorrer a matriz tab
-        if state and ((a) % 3 == 0 and a != 0): #condição para só criar uma barra de três em três números
+        if state and ((a % 3 == 0) and a != 0): #condicao para so criar uma barra de tres em tres numeros
             
             puzzle = puzzle + '\n--------------------------------\n'
             
@@ -113,5 +114,5 @@ def get_tabuleiro(tab):
     return puzzle
 
     
-#lsd = solver(A) 
-#get_tabuleiro(lsd)
+lsd = solver(A) 
+get_tabuleiro(lsd)
