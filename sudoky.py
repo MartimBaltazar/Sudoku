4298a9cf2a9db522acb86c607255fa754175728e
duarte3333 <76222459+duarte3333@users.noreply.github.com>
2021-11-18 13:55:15 +0000

    implemented a function to generate grids

--- sudoky.py
+++ sudoky.py
@@ -1,6 +1,7 @@
 # generate random integer values
 from random import seed
 from random import randint
+import random
 
 #Função responsável por retornar a coluna atual
 def get_coluna(tab, n):
@@ -67,10 +68,12 @@ def empty_spot(grid,l):
                 l[1]= col
                 return True
     return False     
-   
+ 
 #Função que resolve o sudoko
-def solver(tab):
-    
+def solver(tab,i=[0]):
+
+    i[0]+=1
+
     l =[0, 0] #Inicialização do indice das linhas e das colunas
     
     if (empty_spot(tab,l) == False):
@@ -83,7 +86,7 @@ def solver(tab):
 
             if is_safe(tab,row,col,i) : #Verifica se o número i passa nas 
                 #condições básicas do jogo do Sudoko: ser único na linha, na coluna e na célula
-                    
+                   
                 tab[row][col] = i   #Caso passe nas condições acima, coloca esse número i na entrada row,col do grid e depois 
                 #chama-se a função solver() de novo com o grid atualizado para esta entrada row col e tenta-se preencher 
                 # a próxima entrada que esteja a zero até ver se é possível preencher todo o grid.
@@ -91,16 +94,66 @@ def solver(tab):
                 #Caso se chegue a um "dead end" em que fiquem zeros por preencher e já não é possível cumprir as regras, 
                 #repete-se o processo todo de novo porque na linha 98 se dá um reset na entrada inicial desta tentativa de grid resolvido e 
                 #como isto é um loop for, logo a próxima tentativa será diferente e serão tentadas todas as possibilidades.
-        
+                     
                 if(solver(tab)): #Recursiva ocorre,até que um grid seja preenchido sem deixar 
                 #zeros e onde se cumprem as regras básicas do jogo
+                    #print(i[0])
                     return True
                 
                 tab[row][col] = 0 #Isto executa o backtracking 
-             
-        
+                
+                
+    
     return False
-               
+    
+
+
+def criar_tabuleiro(level):
+    grid = []
+
+    lista = [0,1,2,3,4,5,6,7,8,9]
+        
+    #50 pistas e 31 zeros - nivel 1
+    if level == 1:
+        limit = 31
+
+    #35 pistas e 46 zeros - nivel 2
+    elif level == 2:
+        limit = 46
+
+    #20pistas e 61 zeros - nivel 3
+    else:
+        limit = 61
+    
+    grid = [[0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0]]
+
+    #ESTE WHILE DEMOROU 1H A SER FEITOOOOOOOOOOOO
+    #mas funciona bem tho
+    m=100
+    while m > limit:
+        m = 0
+        for i in range(9):
+            m = m + grid[i].count(0)
+        
+        for i in range(9):
+            
+            s = random.randint(0,int(len(lista))-1)
+            a = lista[s]
+            for j in range(9):
+                if is_safe(grid,i,j,a):
+                    grid[i][j] = a
+        
+                        
+    return grid
+                
 #Função que dá print do resultado obtido na função solver
 def get_tabuleiro(tab):
     puzzle = ''
@@ -138,20 +191,24 @@ def get_tabuleiro(tab):
 #get_tabuleiro(lsd)
 
 if __name__=="__main__":
+
+    A = criar_tabuleiro(2)
+    print("Tabuleiro por resolver: \n")
+    get_tabuleiro(A)
+    #A =[[0 for x in range(9)]for y in range(9)]
     
-    A =[[0 for x in range(9)]for y in range(9)]
-    
-    A = [[5,1,7,6,0,0,0,3,4],
-     [2,0,9,0,0,4,0,0,0],
-     [3,4,6,2,0,5,0,9,0],
-     [6,0,2,0,0,0,0,1,0],
-     [0,3,8,0,0,6,0,4,7],
-     [0,0,0,0,0,0,0,0,0],
-     [0,9,0,0,0,0,0,7,8],
-     [7,0,3,4,0,0,5,6,0],
-     [0,0,0,0,0,0,0,0,0]]
+    #A = [[5,1,7,6,0,0,0,3,4],
+    # [2,0,9,0,0,4,0,0,0],
+    # [3,4,6,2,0,5,0,9,0],
+    # [6,0,2,0,0,0,0,1,0],
+    # [0,3,8,0,0,6,0,4,7],
+    # [0,0,0,0,0,0,0,0,0],
+    # [0,9,0,0,0,0,0,7,8],
+    # [7,0,3,4,0,0,5,6,0],
+    # [0,0,0,0,0,0,0,0,0]]
       
     if(solver(A)):
+            print("Tabuleiro Resolvido: \n")
             get_tabuleiro(A)
     else:
             print("No solution exists")
