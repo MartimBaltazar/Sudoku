e7cd8667f581fa7985a26712f0d57bd667a33793
duarte3333 <76222459+duarte3333@users.noreply.github.com>
2021-12-09 02:02:56 +0000

    the neural network is working now

--- ai_solver.py
+++ ai_solver.py
@@ -9,6 +9,16 @@ from tensorflow import keras
 from tensorflow.keras.models import load_model
 from tensorflow.keras.datasets import mnist
 import os
+import PIL
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageOps
+import tensorflow as tf
+from keras.layers import Rescaling, Conv2D, Flatten, Dropout, MaxPooling2D, Dense
+import os
+
+
+
 current_dir = os.getcwd()# Import mnist data stored in the following path: current directory -> mnist.npz
 
 
@@ -65,12 +75,55 @@ def main_ai():
 #main_ai()
 
 # so usar se o tensorflow funcionar
+def ai_maker():
+    train_new_model = True
+
+    if train_new_model:
+        #data = tf.keras.datasets.mnist
+        (train_images, train_labels), (test_images, test_labels) = mnist.load_data(path=current_dir+'/mnist.npz')
+
+        unique, counts = np.unique(train_labels, return_counts=True)
+        print(dict(zip(unique, counts)))
+
+        
+        model = tf.keras.Sequential([
+            Rescaling(1. / 255, input_shape=(28, 28, 1)),
+            Conv2D(32, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Conv2D(64, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Conv2D(128, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            Flatten(),
+            Dense(256, activation='relu'),
+            Dropout(0.5),
+            Dense(10, activation='softmax')])
+
+
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        model.fit(train_images, train_labels, epochs=2)
+
+        model.save_weights('./model')
+
+        teste_loss, test_acc = model.evaluate(test_images, test_labels)
+        
+        #model.save('handwritten_digits.model')
+
+    else:
+        # Load the model
+        model = tf.keras.models.load_model('handwritten_digits.model')
+    
+    return model 
+
+
 
 def flow_ai():
     
-    model = treinar()
+    #model = treinar()
+    model = ai_maker()
         
-    image_path = '4.png'
+    image_path = 'oi.png'
     image_pre_AI = grid_pronto_para_AI(image_path)
     image_pre_AI_2 = cv2.resize(image_pre_AI, (450,450))
     #quadrado1 = image_pre_AI_2[(int(450/3)):(int(450/9)),(int(450/3)):(int((450*3)/(9)))]
@@ -84,24 +137,27 @@ def flow_ai():
         for j in range(9):
             #image = image_pre_AI_2[i*50:(i+1)*50,j*50:(j+1)*50]
             if image_pre_AI_2.sum() > 25000:    
-
+                
                 quadrado_atual = image_pre_AI_2[(int(450*(i)/9)):(int((450*(i+1))/9)),(int((450*(j))/9)):(int((450*(j+1))/9))]
+              
+                #quadrado_atual = image_pre_AI_2[(int(450*(i)/9)):(int((450*(i+1))/9)),(int((450*(j))/9)):(int((450*(j+1))/9))]
                 number_of_black_pix = np.sum(quadrado_atual== 0)
                 nr = int(number_of_black_pix)
                 max1.append(nr)
                 
     maxi = max(max1)
     grid = np.zeros([9,9])
+    
     for i in range(9):
         for j in range(9):
             #image = image_pre_AI_2[i*50:(i+1)*50,j*50:(j+1)*50]
-            if image_pre_AI_2.sum() > 25000:    
-
-                quadrado_atual = image_pre_AI_2[(int(450*(i)/9)):(int((450*(i+1))/9)),(int((450*(j))/9)):(int((450*(j+1))/9))]
+            if image_pre_AI_2.sum() > 25000:   
+                 
+                quadrado_atual = image_pre_AI_2[(int(450*(i)/9))+3:(int((450*(i+1))/9))-3,(int((450*(j))/9))+3:(int((450*(j+1))/9))-3] 
                 number_of_black_pix = np.sum(quadrado_atual== 0)
                 #print(str(maxi) + "/" + str(number_of_black_pix))
     
-                if number_of_black_pix == maxi:
+                if number_of_black_pix == (maxi-180):
                     grid[i][j] = 0
                     #print("deu zero")
                 else:
@@ -110,7 +166,13 @@ def flow_ai():
                     #mostrar_imagem(image_resize)
                     image_resize_2 = np.reshape(image_resize, (1,28,28))
                     #mostrar_imagem(image_resize_2)
-                    grid[i][j] = np.argmax(model.predict(image_resize_2))
+                    #a = (class_names[i], float(prediction[0][i] * 100))
+                    final =  np.argmax(model.predict(image_resize_2)) 
+                    if (model.predict(image_resize_2)[0][final] > 0.65):
+                        grid[i][j] = final
+                    #plt.imshow(image_resize_2[0])
+                    #plt.show()
+
                     
                     #print("The number is probably a {}".format(np.argmax(grid[i][j])))
                     #plt.imshow(quadrado_atual[0], cmap=plt.cm.binary)
